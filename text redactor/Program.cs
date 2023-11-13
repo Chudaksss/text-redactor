e882c58e891ed29eca6aab00a6e6ddce6d9e4359
Chudaks <isip_i.d.lunev@mpt.ru>
2023-11-13 19:44:18 +0300

    Добавьте файлы проекта.

---
+++ text redactor/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace TextEditor
+{
+    public class Figure
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VVEDITE PUTb FAILA:");
+            string filePath = Console.ReadLine(); // выбор файла по его пути
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File ne naiden");
+                return;
+            }
+
+            // загрузка информации из файла
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.F1)
+                {
+                    // сохранение файла
+                    string fileExtension = Path.GetExtension(filePath).ToLower();
+
+                    if (fileExtension == ".txt")
+                    {
+                        File.WriteAllLines(filePath, lines);
+                    }
+                    else if (fileExtension == ".json")
+                    {
+                        // преобразование модели в JSON и сохранение
+                        string jsonData = JsonConvert.SerializeObject(lines);
+                        File.WriteAllText(filePath, jsonData);
+                    }
+                    else if (fileExtension == ".xml")
+                    {
+                        // преобразование модели в XML и сохранение
+                        XmlSerializer xmlSerializer = new XmlSerializer(typeof(string[]));
+                        using (TextWriter writer = new StreamWriter(filePath))
+                        {
+                            xmlSerializer.Serialize(writer, lines);
+                        }
+                    }
+
+                    Console.WriteLine("File sohranen");
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
